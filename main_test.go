76733d3a7d1902df7265dae1324277addad79e3f
TaikiSugiyama <homenobiz@gmail.com>
2018-04-29 03:02:13 +0900

    add: test for retrieve content type

--- main_test.go
+++ main_test.go
@@ -1,21 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestRetrieveMethodRequestLine(t *testing.T) {
-	if retrieveMethodRequestLine("GET /test HTTP1.0") != "GET" {
+	if retrieveMethodRequestLine("GET /test.html HTTP1.0") != "GET" {
 		t.Fatal("retrieveMethodRequestLine should be GET")
 	}
 }
 
 func TestRetrieveURIRequestLinet(t *testing.T) {
-	if retrieveURIRequestLine("GET /test HTTP1.0") != "/test" {
+	if retrieveURIRequestLine("GET /test.html HTTP1.0") != "/test.html" {
 		t.Fatal("retrieveURIRequestLine should be /test")
 	}
 }
 
 func TestRetrieveVersionRequestLine(t *testing.T) {
-	if retrieveVersionRequestLine("GET /test HTTP1.0") != "HTTP1.0" {
+	if retrieveVersionRequestLine("GET /test.html HTTP1.0") != "HTTP1.0" {
 		t.Fatal("retrieveVersionRequestLine should be HTTP1.0")
+	}
+}
+
+func TestRetrieveTypeRequestLine(t *testing.T) {
+	if retrieveTypeRequestLine("/test.html") != ".html" {
+		t.Fatal("retrieveTypeRequestLine should be .html")
 	}
 }
