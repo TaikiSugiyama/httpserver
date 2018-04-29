dcd5c2830e5e8e40adbace337447dc89e8b44227
TaikiSugiyama <homenobiz@gmail.com>
2018-04-29 17:11:37 +0900

    add: head method receivor

--- main.go
+++ main.go
@@ -147,6 +147,21 @@ func returnResponse(conn *net.TCPConn, method string, uri string, ver string, co
 		checkError(err)
 		_, err = conn.Write([]byte(body))
 		checkError(err)
+	case "HEAD":
+		if _, err := os.Stat(filePath); err != nil {
+			_, err := conn.Write([]byte(ver + " 404 Not Found\n"))
+			checkError(err)
+			break
+		}
+		// status line
+		_, err := conn.Write([]byte(ver + " 200 OK\n"))
+		checkError(err)
+		_, err = conn.Write([]byte("\n"))
+		checkError(err)
+		body, err := ioutil.ReadFile(filePath)
+		checkError(err)
+		_, err = conn.Write([]byte(body))
+		checkError(err)
 	default:
 		_, err := conn.Write([]byte(ver + " 405 Method Not Allowed\n"))
 		checkError(err)
